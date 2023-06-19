237503c62a891b23e46d08a2b55f6ba8b172d248
arky <847566733@qq.com>
2023-06-19 14:22:24 +0800

    删除自己写的一些组件，便于更存粹查看React在浏览器中方法的调用栈

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './page/App';
 
 
-render(<App />, document.getElementById('root'));
+// render(<App />, document.getElementById('root'));
+/**
+ *  Concurrent Mode 并发模式
+ */
+createRoot(document.getElementById('root')).render(<App />);
--- src/page/App.tsx
+++ src/page/App.tsx
@@ -1,16 +1,8 @@
-import * as  React from 'react';
-import { useState } from '../../packages/react-18.2.0/packages/react/src/ReactHooks';
+import * as React from 'react';
+
 
 const App = () => {
-    const [count, setCount] = useState(1);
-    const increase = () => {
-        setCount(count + 1);
-    }
-    return <div>
-        {count}
-        <br />
-        <button onClick={increase}>数字加1</button>
-    </div>
+    return <div>初识React源码</div>
 }
 
 
